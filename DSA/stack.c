08e5c369d3066f5b6a05c0cbb56c3f5d82d070f9
AmanThakur01 <amanmrthakur@gmail.com>
2021-10-12 16:50:02 +0530

    pop,push,pull stack

--- DSA/stack.c
+++ DSA/stack.c
@@ -31,6 +31,33 @@ int isFull(struct stack *ptr)
         return 0;
     }
 }
+int pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack Underflow! Cannot pop from the stack\n");
+        return -1;
+    }
+    else
+    {
+        int val = ptr->arr[ptr->top];
+        ptr->top--;
+        return val;
+    }
+}
+
+void push(struct stack *ptr, int val)
+{
+    if (isFull(ptr))
+    {
+        printf("Stack Overflow! Cannot push %d to the stack\n", val);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = val;
+    }
+}
 
 int main()
 {
@@ -59,4 +86,10 @@ int main()
     {
         printf("The stack is not empty");
     }
+    //this is new
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+    printf("Stack has been created successfully\n");
 }
